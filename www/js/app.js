580cb718a0a6798291aaecd890cbc122b7866f81
Vijay Tilak <vijaykrishnatilak@gmail.com>
2016-05-12 18:02:56 +1200

    Added Lock Screen to Portrait Mode

--- www/js/app.js
+++ www/js/app.js
@@ -7,17 +7,25 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+  .controller('mainCtrl', function ($scope) {
+    document.addEventListener("deviceready", onDeviceReady, false);
+    function onDeviceReady() {
+      //Locks Screen in Portrait Mode
+      screen.lockOrientation('portrait');
     }
   });
-})
--- www/js/routes.js
+++ www/js/routes.js
@@ -1,93 +1,89 @@
 angular.module('app.routes', [])
 
-.config(function($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-    
-  
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
       .state('tabsController.apps', {
-    url: '/apps',
-    views: {
-      'tab1': {
-        templateUrl: 'templates/apps.html',
-        controller: 'appsCtrl'
-      }
-    }
-  })
+        url: '/apps',
+        views: {
+          'tab1': {
+            templateUrl: 'templates/apps.html',
+            controller: 'appsCtrl'
+          }
+        }
+      })
 
-  .state('tabsController.siteSurvey', {
-    url: '/sitesurvey',
-    views: {
-      'tab1': {
-        templateUrl: 'templates/siteSurvey.html',
-        controller: 'siteSurveyCtrl'
-      }
-    }
-  })
+      .state('tabsController.siteSurvey', {
+        url: '/sitesurvey',
+        views: {
+          'tab1': {
+            templateUrl: 'templates/siteSurvey.html',
+            controller: 'siteSurveyCtrl'
+          }
+        }
+      })
 
-  .state('tabsController.surveyCategory', {
-    url: '/surveycategory',
-    views: {
-      'tab1': {
-        templateUrl: 'templates/surveyCategory.html',
-        controller: 'surveyCategoryCtrl'
-      }
-    }
-  })
+      .state('tabsController.surveyCategory', {
+        url: '/surveycategory',
+        views: {
+          'tab1': {
+            templateUrl: 'templates/surveyCategory.html',
+            controller: 'surveyCategoryCtrl'
+          }
+        }
+      })
 
-  .state('tabsController.c1Q1', {
-    url: '/c1q1',
-    views: {
-      'tab1': {
-        templateUrl: 'templates/c1Q1.html',
-        controller: 'c1Q1Ctrl'
-      }
-    }
-  })
+      .state('tabsController.c1Q1', {
+        url: '/c1q1',
+        views: {
+          'tab1': {
+            templateUrl: 'templates/c1Q1.html',
+            controller: 'c1Q1Ctrl'
+          }
+        }
+      })
 
-  .state('tabsController.diagram', {
-    url: '/diagram',
-    views: {
-      'tab1': {
-        templateUrl: 'templates/diagram.html',
-        controller: 'diagramCtrl'
-      }
-    }
-  })
+      .state('tabsController.diagram', {
+        url: '/diagram',
+        views: {
+          'tab1': {
+            templateUrl: 'templates/diagram.html',
+            controller: 'diagramCtrl'
+          }
+        }
+      })
 
-  .state('tabsController.updates', {
-    url: '/updates',
-    views: {
-      'tab2': {
-        templateUrl: 'templates/updates.html',
-        controller: 'updatesCtrl'
-      }
-    }
-  })
+      .state('tabsController.updates', {
+        url: '/updates',
+        views: {
+          'tab2': {
+            templateUrl: 'templates/updates.html',
+            controller: 'updatesCtrl'
+          }
+        }
+      })
 
-  .state('tabsController.office', {
-    url: '/office',
-    views: {
-      'tab3': {
-        templateUrl: 'templates/office.html',
-        controller: 'officeCtrl'
-      }
-    }
-  })
+      .state('tabsController.office', {
+        url: '/office',
+        views: {
+          'tab3': {
+            templateUrl: 'templates/office.html',
+            controller: 'officeCtrl'
+          }
+        }
+      })
 
-  .state('tabsController', {
-    url: '/page1',
-    templateUrl: 'templates/tabsController.html',
-    abstract:true
-  })
+      .state('tabsController', {
+        url: '/page1',
+        templateUrl: 'templates/tabsController.html',
+        abstract: true
+      });
 
-$urlRouterProvider.otherwise('/page1/apps')
+    $urlRouterProvider.otherwise('/page1/apps');
 
-  
-
-});
+  });
